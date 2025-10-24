178632c1c03f280e00e8bf3f8143c99ff24e3d04
amirenger <amirabdian00@gmail.com>
2025-10-24 14:44:00 +0000

    do it

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-# اجرای ربات در پس‌زمینه (Polling)
-python app.py &
+# # اجرای ربات در پس‌زمینه (Polling)
+# python app.py &
 
-# اجرای سرور Flask با Gunicorn برای پاسخ به پینگ Render
-gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:$PORT app:app
+# # اجرای سرور Flask با Gunicorn برای پاسخ به پینگ Render
+# gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:$PORT app:app
